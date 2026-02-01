e1b110ce1bd4c55df8916aa22f43bc7a6d4169e2
ipz_242-rdr <ipz242_rdr@student.ztu.edu.ua>
2026-02-01 23:39:56 +0200

    Infrastructure налаштовано підключення до бд

---
+++ ATM.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ATM.Domain.Entities;
+
+namespace ATM.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Card> Cards { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<AtmCassette> AtmCassettes { get; set; }
+        public DbSet<AtmOperationLog> AtmOperationLogs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=localhost;Database=ATM_System;" +
+                    "Trusted_connection=True;TrustServerCertificate=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Entity<Card>().ToTable("Cards");
+            modelBuilder.Entity<Account>().ToTable("Accounts");
+            modelBuilder.Entity<Transaction>().ToTable("Transactions");
+            modelBuilder.Entity<AtmCassette>().ToTable("ATMCassettes");
+            modelBuilder.Entity<AtmOperationLog>().ToTable("ATMOperationLogs");
+        }
+    }
+}
