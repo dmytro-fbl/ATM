ce32ddf113b12e1edb764207b7fce1cb9ad65684
dmytro-fbl <dmytrowork06@gmail.com>
2026-04-07 19:37:49 +0300

    implement ITransactionRepository and TransactionRepository with async LINQ and Guid support

---
+++ ATM.Domain/Interfaces/ITransactionRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATM.Domain.Entities;
+
+
+namespace ATM.Domain.Interfaces
+{
+    public interface ITransactionRepository
+    {
+        Task AddAsync(Transaction transaction);
+
+        Task<Transaction?> GetByIdAsync(Guid id);
+
+        Task<IEnumerable<Transaction>> GetByAccountIdAsync(Guid accountId);
+
+        Task<IEnumerable<Transaction>> GetRecentTransactionAsync(Guid accountId, int count);
+    }
+}
---
+++ ATM.Infrastructure/Repositories/TransactionRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATM.Domain.Entities;
+using ATM.Domain.Interfaces;
+using ATM.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ATM.Infrastructure.Repositories
+{
+    public class TransactionRepository : ITransactionRepository
+    {
+        private readonly AppDbContext _context;
+        public TransactionRepository(AppDbContext context)
+        {
+            _context = context; 
+        } 
+        public async Task AddAsync(Transaction transaction)
+        {
+            _context.Transactions.Add(transaction);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Transaction>> GetByAccountIdAsync(Guid accountId)
+        {
+            return await _context.Transactions
+                .Where(t => t.AccountId == accountId)
+                .ToListAsync();
+        }
+
+        public Task<Transaction?> GetByIdAsync(Guid id)
+        {
+            return _context.Transactions
+               .FirstOrDefaultAsync(t => t.Id == id);
+            
+        }
+
+        public async Task<IEnumerable<Transaction>> GetRecentTransactionAsync(Guid accountId, int count)
+        {
+            return await _context.Transactions
+                .Where(t => t.AccountId == accountId)
+                .OrderByDescending(t => t.TransactionDate)
+                .Take(count)
+                .ToListAsync(); 
+        }
+    }
+}
