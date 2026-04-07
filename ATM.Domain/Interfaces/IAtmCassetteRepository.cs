8bf2f39d2c383eae312dbb70674fd21751fd293e
dmytro-fbl <dmytrowork06@gmail.com>
2026-04-07 21:10:49 +0300

    implement IAtmCassetteRepository and AtmCassetteRepository

---
+++ ATM.Domain/Interfaces/IAtmCassetteRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATM.Domain.Entities;
+
+namespace ATM.Domain.Interfaces
+{
+    public interface IAtmCassetteRepository
+    {
+        Task<IEnumerable<AtmCassette>> GetAllAsync();
+        Task<AtmCassette?> GetByDenominationAsync(int denomitation);
+        Task UpdateAsync(AtmCassette cassette);
+    }
+}
---
+++ ATM.Infrastructure/Repositories/AtmCassetteRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATM.Domain.Entities;
+using ATM.Domain.Interfaces;
+using ATM.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ATM.Infrastructure.Repositories
+{
+    public class AtmCassetteRepository : IAtmCassetteRepository
+    {
+        private readonly AppDbContext _context;
+
+        public AtmCassetteRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<AtmCassette>> GetAllAsync()
+        {
+            return await _context.AtmCassettes
+                .ToListAsync();
+        }
+
+        public async Task<AtmCassette?> GetByDenominationAsync(int denomination)
+        {
+            return await _context.AtmCassettes
+                .FirstOrDefaultAsync(a => a.Denomination == denomination);
+        }
+
+        public async Task UpdateAsync(AtmCassette cassette)
+        {
+            _context.AtmCassettes.Update(cassette);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
