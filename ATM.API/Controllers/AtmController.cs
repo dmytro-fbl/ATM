aeace9c152141f4add359023aaef2a70ada3ad4e
dmytro-fbl <dmytrowork06@gmail.com>
2026-04-13 16:40:46 +0300

    created DTOs to store data needed for querying and creating a logging controller

---
+++ ATM.API/Controllers/AtmController.cs
@@ -0,0 +1,33 @@
+﻿using ATM.API.DTOs;
+using ATM.Domain.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ATM.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AtmController : ControllerBase
+    {
+        private readonly IAtmService _atmService;
+
+        public AtmController(IAtmService atmService)
+        {
+            _atmService = atmService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            bool isSuccess = await _atmService.AuthenticateAsync(request.CardNumber, request.Pin);
+
+            if (isSuccess)
+            {
+                return Ok(new {message = "Успішний вхід", isAuthentificated = true});
+            }
+            else
+            {
+                return Unauthorized(new { message = "невірний номер картки або ПІН-код" });
+            }
+        }
+    }
+}
